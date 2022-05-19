a392eb442f7fbb301c3b9c197382ec559fae07da
Allister William Gray Gardner <allister.w.g.gardner@gmail.com>
2022-05-19 15:58:46 +0100

    taken user login stuff out for now

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -3,12 +3,13 @@ import React from "react";
 import EmailForm from "./EmailForm";
 import Navbar from "./navbar";
 import Footer from "./footer";
+import Header from "./Header";
 
 
 
 const ContactPage = () => {
     return (
-        <><Navbar /><EmailForm /><Footer /></>
+        <><Header /><Navbar /><EmailForm /><Footer /></>
     );
 }
 
