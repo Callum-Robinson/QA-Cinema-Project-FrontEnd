077bbf36546aa6df319898e137bbf4a2b5e56d6c
SalahS49 <salah_49@outlook.com>
2022-05-20 15:06:51 +0100

    added main tags

--- src/Components/About/OurTeam.jsx
+++ src/Components/About/OurTeam.jsx
@@ -1,12 +1,12 @@
-import Salah from "./Creators/Salah"; 
+import Salah from "./Creators/Salah";
 import Allister from "./Creators/Allister";
 import Mohab from "./Creators/Mohab";
 import Callum from "./Creators/Callum";
 const OurTeam = () => {
-
-    return (
-        <>
-        <h1 > Our Team </h1>
+  return (
+    <>
+      <main>
+        <h1> Our Team </h1>
         <hr />
         <br></br>
 
@@ -15,9 +15,9 @@ const OurTeam = () => {
         <Mohab />
         <Callum />
         <hr />
-        </>
-    )
-
-}
+      </main>
+    </>
+  );
+};
 
 export default OurTeam;
