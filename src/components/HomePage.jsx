f060e180b4224af0467d53f81e074e8e91b0e9b5
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-20 11:50:20 +0100

    added shell for home page

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Navbar from "./navbar";
+import Footer from "./Footer";
+import Header from "./Header";
+
+
+
+const HomePage = () => {
+    return (
+        <><Header /><Navbar /><Footer /></>
+    );
+}
+
+
+export default HomePage;
