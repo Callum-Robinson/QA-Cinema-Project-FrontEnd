dfb7e705e5592c0f68b0d2d0c39deec44da64166
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-20 16:52:57 +0100

    added route import for Home

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ContactPage from './Components/Contact/ContactPage';
-
+import HomePage from './Components/Home/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
