4e8034ea6ba5ba38a58ceb03f5e27cd957f0d056
SalahS49 <salah_49@outlook.com>
2022-05-20 11:29:28 +0100

    created about page files

---
+++ src/Components/About/Creators/Salah.jsx
@@ -0,0 +1,17 @@
+
+
+const Salah = () => {
+
+return (
+    <>
+    <h1> Salah </h1>
+    <p>
+        Hi my name is Salah, and I am one of the creators of QA Cinemas!
+    </p>
+    <img src="/res/Salah.jpg"/>
+    </>
+)
+
+}
+
+export default Salah;
---
+++ src/Components/About/OurTeam.jsx
@@ -0,0 +1,18 @@
+import Salah from "./Creators/Salah"; 
+
+const OurTeam = () => {
+
+    return (
+        <>
+        <h1 > Our Team </h1>
+        <hr />
+        <br></br>
+
+        <Salah />
+        <hr />
+        </>
+    )
+
+}
+
+export default OurTeam;
