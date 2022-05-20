4ee7eb1870162e2d883653494da9045a0572eac5
Callum <callumjrobinson2020@gmail.com>
2022-05-20 16:06:14 +0100

    update a path to use changed component file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ContactPage from './components/ContactPage';
+import ContactPage from './Components/ContactPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
