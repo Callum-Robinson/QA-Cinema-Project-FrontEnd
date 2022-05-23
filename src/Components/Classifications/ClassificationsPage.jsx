0cc79d73ab2cb387b17bfdb2654e38301280d34d
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-23 10:39:19 +0100

    created png for bbfc symbols and added it to page

---
+++ src/Components/Classifications/ClassificationsPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+export default function Classifications() {
+    return (
+        <main>
+            <h2>Classifications</h2>
+
+            <img id="classifications" src="/images/all-digital-bbfc-symbols.png" alt="placeholder logo" />
+        </main>
+
+            )
+}
