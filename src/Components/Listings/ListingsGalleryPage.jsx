bfaa7c07a293c90260cb8676aa2c0339b7a9a79f
Callum <callumjrobinson2020@gmail.com>
2022-05-23 16:33:06 +0100

    FPC-14 #comment started movie details component

--- src/Components/Listings/ListingsGalleryPage.jsx
+++ src/Components/Listings/ListingsGalleryPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 
-function ListingsGalleryPage(props) {
+const ListingsGalleryPage = () => {
 
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
--- src/Components/MovieDetails/MovieDetails.jsx
+++ src/Components/MovieDetails/MovieDetails.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
 
 
 const MovieDetails = () => {
+    const params = useParams();
+
+
     return (
         <main>
             <h2>Movie Details</h2>
