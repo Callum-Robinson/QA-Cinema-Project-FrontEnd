74ac875f56fc5fddb691329f60b55c7b0c764373
Callum <callumjrobinson2020@gmail.com>
2022-05-23 15:15:13 +0100

    added map movie images function

--- src/Components/Listings/ListingsGalleryPage.jsx
+++ src/Components/Listings/ListingsGalleryPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import { Link, Outlet } from "react-router-dom";
 
 
 function ListingsGalleryPage(props) {
@@ -7,6 +7,21 @@ function ListingsGalleryPage(props) {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    const mapMovieImages = (movie) => {
+        return (
+            <div key={movie.title}>
+                <Link to={`/listingsgallery/${movie.id}`}>
+                    <img id="movie-poster" src={movie.image} alt="movie poster" />
+                </Link>
+                <h3 id="movie-listing-title">{movie.title}</h3>
+            </div>
+        )
+    }
+
+
+
+
 
     return (
         <main>
