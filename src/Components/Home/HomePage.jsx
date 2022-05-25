4d0cba57e377ceb421690452a2d2ca3ac0e7d1c1
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-25 12:02:52 +0100

    cleaned up some of the css for home

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -7,7 +7,7 @@ export default function Home() {
         
         <main>
             <h2>WELCOME TO QA CINEMAS!</h2>
-            <br/><br/>
+            
             
                 <div id="home-content">
                     
@@ -22,7 +22,7 @@ export default function Home() {
 <p> QA Cinemas is a brand new venture that hopes to lead the way in both specialised and general audience cinema in the years to come.<br/><br/> Our screens show all the hottest releases, specialised titles, first run world and independent cinema, artists’ experimental work, issue based programmes, thematic seasons, repertory programmes, Scottish and UK produced work, festivals, and a programme of event cinema and live broadcast. <br/><br/>
 
 We provide an independent film programme for diverse audiences in Glasgow and soon more locations in the UK, including specific audience development initiatives aimed at cultural diversity, Deaf and hard of hearing audiences, and socially/economically disadvantaged audiences. QA Cinemas thrives in a highly competitive cinema market for cinema by providing a clearly differentiated experience and programme.
-</p> <br/><br/><br/><br/><br/><br/><br/>
+</p> <br/><br/>
     
                 </div>
         </main>
