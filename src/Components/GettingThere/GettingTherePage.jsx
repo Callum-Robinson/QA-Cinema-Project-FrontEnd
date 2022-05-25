6db77197e4b0fc4ee6330ec81d0e1f0ba4bc1bd5
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-25 11:06:24 +0100

    added images to getting there page (image of the cinema - taken from google maps of the qa office in Glasgow, map showing where it is and a gif with directions from transport links)

--- src/Components/GettingThere/GettingTherePage.jsx
+++ src/Components/GettingThere/GettingTherePage.jsx
@@ -6,7 +6,11 @@ const GettingTherePage = () => {
     return (
         <main>
             <h2>Getting There</h2>
-
+            <div id="getting-there">
+            <img id="cinema" src="/images/cinema.png" alt="a photograph of our Glasgow cinema from the street" />
+            <img id="map" src="/images/map.png" alt="QA Cinemas" />
+            <img id="directions" src="/images/directions.gif" alt="QA Cinemas" />
+            </div>
             
         </main>
 
