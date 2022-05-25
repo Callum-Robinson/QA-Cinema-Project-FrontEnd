3441ca76e1f3635c334bbd9f9c44a6f1c6bec473
Mohab <khalifamohab97@outlook.com>
2022-05-25 11:12:01 +0100

    added ticket booking page

--- src/Components/TicketBooking/MainPage/TicketBookingPage.jsx
+++ src/Components/TicketBooking/MainPage/TicketBookingPage.jsx
@@ -1,16 +1,32 @@
-import React from "react";
-
+import React from "react"
+import { Alert } from 'reactstrap'
 
 
 const TicketBookingPage = () => {
+
+    const [basketInfo, setbasketinfo] = useState({});
+    const [ticketAlert, setticketAlert] = useState(false);
+
     return (
-        <main>
-            <h2>Ticket Booking</h2>
-
-            
-        </main>
-
-            )
+        <>
+            <hr />
+            <div className="row">
+                <div className="col-9">
+                    <br />
+                    <h2 style={{ color: "gold", marginLeft: "25px" }}><center>Ticket Booking:</center></h2>
+                    <Alert isOpen={ticketAlert} style={{ marginLeft: "25px", textAlign: "center" }} color="danger">Please select a movie and seats before purchasing!</Alert>
+                    <br />
+                    <br />
+                    <div className="">
+                        <ReadAll basketinfo={setbasketinfo} />
+                    </div>
+                </div>
+                <div className="col-3" style={{ backgroundColor: "black" }}>
+                    <Sidebar ticketalert={setticketAlert} basketinfo={basketInfo} style={{ backgroundColor: "black" }} />
+                </div>
+            </div>
+        </>
+    )
 };
 
 export default TicketBookingPage;
