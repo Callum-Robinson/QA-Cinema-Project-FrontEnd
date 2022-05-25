1e74a0dd022c592a72dc2ac92536de3b91cf6833
SalahS49 <salah_49@outlook.com>
2022-05-26 00:26:50 +0100

    created classification files

---
+++ src/Components/Classifications/ClassificationsLinks.jsx
@@ -0,0 +1,20 @@
+const ClassificationsLinks = () => {
+  return (
+    <div>
+      <h3> Classifications Links </h3>
+
+      <p>
+        <hr /> To find out more information about film classifications click{" "}
+        <a href="https://www.bbfc.co.uk/about-classification/classification-guidelines">
+          here
+        </a>
+        . This external will allow you to understand more about why it is
+        important to know when booking a film at our cinema, that it is in line
+        with the age restrictions and you are happy with the classification
+        rules.
+      </p>
+    </div>
+  );
+};
+
+export default ClassificationsLinks;
