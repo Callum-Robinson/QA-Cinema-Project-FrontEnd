5178314752c63c5bed230a21b475854e203dfd34
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-25 14:26:55 +0100

    added maps and directions to getting there

--- src/Components/GettingThere/GettingTherePage.jsx
+++ src/Components/GettingThere/GettingTherePage.jsx
@@ -5,16 +5,52 @@ import React from "react";
 const GettingTherePage = () => {
     return (
         <main>
-            <h2>Getting There</h2>
-            <div id="getting-there">
-            <img id="cinema" src="/images/cinema.png" alt="a photograph of our Glasgow cinema from the street" />
-            <img id="map" src="/images/map.png" alt="QA Cinemas" />
-            <img id="directions" src="/images/directions.gif" alt="QA Cinemas" />
-            </div>
             
+                <div class="clearfix">
+                    <h2>Getting There</h2>
+
+                    
+
+                        <br />
+                        
+                        <h3>Address:</h3>
+                        QA Cinema Glasgow<br />
+                        17 Renfield Street<br />
+                        Glasgow<br />
+                        Scotland<br />
+                        G2 5AH<br />
+                        <br />
+                </div>
+
+
+
+
+
+                <div class="clearfix">
+                    <h3>Where to find us:</h3>
+                    <img id="cinema" src="/images/cinema.png" alt="a photograph of our Glasgow cinema from the street" />
+                    <img id="map" src="/images/map.png" alt="QA Cinemas" />
+
+                </div>
+
+                <div class="clearfix">
+                    <h3>How to get here</h3><br />
+                    <img id="directions" src="/images/directions.gif" alt="QA Cinemas" />
+                    <div class="right-text">
+                    <p>We're right in the city centre, so we're easy to get to! <br /><br />There are lots of public transport options available. <br /><br />We're just a short walk away from both Queen Street and Central Station as well as Buchanan Street Bus Station.
+                        <br /><br /> <h3>DIRECTIONS:</h3>
+                        Click <a href="#">here</a> for directions from QUEEN ST STATION<br /><br />
+                        Click <a href="#">here</a> for directions from CENTRAL STATION<br /><br />
+                        Click <a href="#">here</a> for directions from BUCHANAN ST BUS STATION<br /><br />
+                    </p>
+                    </div>
+                </div>
+            
+
+
         </main>
 
-            )
+    )
 };
 
 export default GettingTherePage;
