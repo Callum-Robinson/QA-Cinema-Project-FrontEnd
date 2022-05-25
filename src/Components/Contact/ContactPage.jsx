59353e69c59501fc9c8e78d2f68d37523f12343e
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-25 14:38:27 +0100

    added info to contact page

--- src/Components/Contact/ContactPage.jsx
+++ src/Components/Contact/ContactPage.jsx
@@ -6,6 +6,16 @@ const ContactPage = () => {
     return (
         <>  
             <main>
+                <h3>Address:</h3>
+                        QA Cinema Glasgow<br />
+                        17 Renfield Street<br />
+                        Glasgow<br />
+                        Scotland<br />
+                        G2 5AH<br />
+                        <br />
+                <h3>Give us a phone:</h3>
+                <a href="tel:03450747851">03450 747 851</a><br/>
+                <h3>Send us an email:</h3>
             <EmailForm />
             </main>
         </>
