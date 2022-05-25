fca8c9cb28bac3b9f632f1f0f8469d0480dde7be
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-25 15:33:20 +0100

    cleaned up layout in css

--- src/Components/Contact/ContactPage.jsx
+++ src/Components/Contact/ContactPage.jsx
@@ -4,19 +4,24 @@ import EmailForm from "./EmailForm";
 
 const ContactPage = () => {
     return (
-        <>  
+        <>
             <main>
-                <h3>Address:</h3>
-                        QA Cinema Glasgow<br />
-                        17 Renfield Street<br />
-                        Glasgow<br />
-                        Scotland<br />
-                        G2 5AH<br />
-                        <br />
-                <h3>Give us a phone:</h3>
-                <a href="tel:03450747851">03450 747 851</a><br/>
-                <h3>Send us an email:</h3>
-            <EmailForm />
+                <div class="clearfix">
+                    <img id="cinema2" src="/images/cinema.png" alt="a photograph of our Glasgow cinema from the street" />
+                    <br /><br />
+                    <h3>Address:</h3>
+                    QA Cinema Glasgow<br />
+                    17 Renfield Street<br />
+                    Glasgow<br />
+                    Scotland<br />
+                    G2 5AH<br />
+                    <br />
+                    <h3>Give us a phone:</h3>
+                    <a href="tel:03450747851">03450 747 851</a><br />
+                    <h3>Send us an email:</h3>
+
+                    <EmailForm />
+                </div>
             </main>
         </>
     );
--- src/Components/GettingThere/GettingTherePage.jsx
+++ src/Components/GettingThere/GettingTherePage.jsx
@@ -7,7 +7,9 @@ const GettingTherePage = () => {
         <main>
             
                 <div class="clearfix">
+                <div class="centered-text">
                     <h2>Getting There</h2>
+                    <img id="cinema" src="/images/cinema.png" alt="a photograph of our Glasgow cinema from the street" />
 
                     
 
@@ -23,20 +25,10 @@ const GettingTherePage = () => {
                 </div>
 
 
-
-
-
-                <div class="clearfix">
-                    <h3>Where to find us:</h3>
-                    <img id="cinema" src="/images/cinema.png" alt="a photograph of our Glasgow cinema from the street" />
-                    <img id="map" src="/images/map.png" alt="QA Cinemas" />
-
-                </div>
-
                 <div class="clearfix">
                     <h3>How to get here</h3><br />
                     <img id="directions" src="/images/directions.gif" alt="QA Cinemas" />
-                    <div class="right-text">
+                    
                     <p>We're right in the city centre, so we're easy to get to! <br /><br />There are lots of public transport options available. <br /><br />We're just a short walk away from both Queen Street and Central Station as well as Buchanan Street Bus Station.
                         <br /><br /> <h3>DIRECTIONS:</h3>
                         Click <a href="#">here</a> for directions from QUEEN ST STATION<br /><br />
