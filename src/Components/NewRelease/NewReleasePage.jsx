44fc42c299534204a65a2657378c29a1f5b7038e
Callum <callumjrobinson2020@gmail.com>
2022-05-26 09:49:51 +0100

    added the state needed for the fetch requests

--- src/Components/NewRelease/NewReleasePage.jsx
+++ src/Components/NewRelease/NewReleasePage.jsx
@@ -4,6 +4,11 @@ import { Link, Outlet } from "react-router-dom";
 
 
 const NewReleasePage = () => {
+
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    
     return (
         <main>
             <h2>New Releases</h2>
