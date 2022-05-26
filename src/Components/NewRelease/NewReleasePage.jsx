029cf1575b8a96e3eaf9d166df3b7aad9df02fc4
Callum <callumjrobinson2020@gmail.com>
2022-05-26 09:52:14 +0100

    added conversion from base64 string to image for release posters

--- src/Components/NewRelease/NewReleasePage.jsx
+++ src/Components/NewRelease/NewReleasePage.jsx
@@ -8,7 +8,12 @@ const NewReleasePage = () => {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
-    
+
+    const mapNewReleaseImages = (release) => {
+        const base64Poster = `data:${release.poster.contentType};base64, ${release.poster.data}`;
+        
+    }
+
     return (
         <main>
             <h2>New Releases</h2>
