fb021fddffa373b772e33c5064c501100b97c96f
Callum <callumjrobinson2020@gmail.com>
2022-05-26 11:11:47 +0100

    added needed state and params for new release details component

--- src/Components/NewRelease/NewReleaseDetails.jsx
+++ src/Components/NewRelease/NewReleaseDetails.jsx
@@ -2,8 +2,13 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 
-
 const NewReleaseDetails = () => {
+    const params = useParams();
+
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
     return (
         <main>
             <h2>New Release Details</h2>
