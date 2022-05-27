4170b5d7ae8429c5e8b692cb0c6ba480dbe100ef
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-27 10:05:17 +0100

    quick css fix for getting there page

--- src/Components/GettingThere/GettingTherePage.jsx
+++ src/Components/GettingThere/GettingTherePage.jsx
@@ -5,39 +5,41 @@ import React from "react";
 const GettingTherePage = () => {
     return (
         <main>
-            
-                <div class="clearfix">
+
+            <div class="clearfix">
                 <div class="centered-text">
                     <h2>Getting There</h2>
                     <img id="cinema" src="/images/cinema.png" alt="a photograph of our Glasgow cinema from the street" />
 
-                    
 
-                        <br />
-                        
-                        <h3>Address:</h3>
-                        QA Cinema Glasgow<br />
-                        17 Renfield Street<br />
-                        Glasgow<br />
-                        Scotland<br />
-                        G2 5AH<br />
-                        <br />
+
+                    <br />
+
+                    <h3>Address:</h3>
+                    QA Cinema Glasgow<br />
+                    17 Renfield Street<br />
+                    Glasgow<br />
+                    Scotland<br />
+                    G2 5AH<br />
+                    <br />
                 </div>
+            </div>
 
 
-                <div class="clearfix">
-                    <h3>How to get here</h3><br />
+            <div class="clearfix">
+                <div class="centered-text"><h3>How to get here</h3><br />
                     <img id="directions" src="/images/directions.gif" alt="QA Cinemas" />
-                    
+
                     <p>We're right in the city centre, so we're easy to get to! <br /><br />There are lots of public transport options available. <br /><br />We're just a short walk away from both Queen Street and Central Station as well as Buchanan Street Bus Station.
                         <br /><br /> <h3>DIRECTIONS:</h3>
-                        Click <a href="#">here</a> for directions from QUEEN ST STATION<br /><br />
-                        Click <a href="#">here</a> for directions from CENTRAL STATION<br /><br />
-                        Click <a href="#">here</a> for directions from BUCHANAN ST BUS STATION<br /><br />
+                        Click <a href="https://www.google.com/maps/dir/Queen+Street+Station,+Dundas+St,+Glasgow+G1+2AF/Portland+House,+17+Renfield+St,+Glasgow+G2+5AH/@55.861615,-4.2556452,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x4888441ffd832079:0x3bdaf84a05057e82!2m2!1d-4.2510943!2d55.8622754!1m5!1m1!1s0x4888469e153fa985:0xdc045928cceed466!2m2!1d-4.2570401!2d55.8610734!3e2">here</a> for directions from QUEEN ST STATION<br /><br />
+                        Click <a href="https://www.google.com/maps/dir/Glasgow+Central+Station,+Gordon+Street,+Glasgow/Portland+House,+17+Renfield+St,+Glasgow+G2+5AH/@55.8600956,-4.2596772,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x4888469dd79d903b:0xef65bf326caa4a56!2m2!1d-4.2581088!2d55.8591118!1m5!1m1!1s0x4888469e153fa985:0xdc045928cceed466!2m2!1d-4.2570401!2d55.8610734!3e2">here</a> for directions from CENTRAL STATION<br /><br />
+                        Click <a href="https://www.google.com/maps/dir/Buchanan+Street+Bus+Station,+Killermont+Street,+Glasgow/Portland+House,+17+Renfield+St,+Glasgow+G2+5AH/@55.8632429,-4.2592301,16z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x4888441f7f0ab0f3:0xdb05b27ea9ab286b!2m2!1d-4.2519263!2d55.8654076!1m5!1m1!1s0x4888469e153fa985:0xdc045928cceed466!2m2!1d-4.2570401!2d55.8610734!3e2">here</a> for directions from BUCHANAN ST BUS STATION<br /><br />
                     </p>
-                    </div>
                 </div>
-            
+            </div>
+
+
 
 
         </main>
